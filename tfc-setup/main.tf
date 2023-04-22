30f00a6cc2778a40d96dc300fd5caa6457f8c9e3
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-22 21:59:00 +0300

    add tfc variables for aws credentials

--- tfc-setup/main.tf
+++ tfc-setup/main.tf
@@ -4,7 +4,7 @@ resource "tfe_organization" "gitops-org" {
 }
 resource "tfe_project" "gitops-eks" {
   organization = tfe_organization.gitops-org.name
-  name = var.project-name
+  name         = var.project-name
 }
 
 resource "tfe_oauth_client" "gitlab-client" {
@@ -17,11 +17,35 @@ resource "tfe_oauth_client" "gitlab-client" {
 }
 
 resource "tfe_workspace" "eks" {
-  name         = "eks"
-  organization = tfe_organization.gitops-org.name
-  project_id = tfe_project.gitops-eks.id
+  name              = "eks"
+  organization      = tfe_organization.gitops-org.name
+  project_id        = tfe_project.gitops-eks.id
+  terraform_version = "~>1.4.5"
+  working_directory = "./eks-argocd"
   vcs_repo {
-      identifier = var.repo-identifier
-      oauth_token_id = tfe_oauth_client.gitlab-client.oauth_token_id
+    identifier     = var.repo-identifier
+    oauth_token_id = tfe_oauth_client.gitlab-client.oauth_token_id
   }
+}
+
+resource "tfe_variable" "aws-region" {
+  key          = "AWS_DEFAULT_REGION"
+  value        = var.AWS_DEFAULT_REGION
+  category     = "env"
+  workspace_id = tfe_workspace.eks.id
+}
+
+resource "tfe_variable" "aws-access-key" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = var.AWS_ACCESS_KEY_ID
+  category     = "env"
+  workspace_id = tfe_workspace.eks.id
+}
+
+resource "tfe_variable" "aws-secret-key" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = var.AWS_SECRET_ACCESS_KEY
+  category     = "env"
+  workspace_id = tfe_workspace.eks.id
+  sensitive    = true
 }
--- tfc-setup/variables.tf
+++ tfc-setup/variables.tf
@@ -1,15 +1,41 @@
 variable "org-name" {
-  type    = string
+  type        = string
+  description = "Your TFC Organization's name."
 }
+
 variable "org-admin" {
-  type    = string
+  type        = string
+  description = "Email address of your TFC Organization's admin."
 }
+
 variable "project-name" {
-  type    = string
+  type        = string
+  description = "Name for a dedicated TFC Project."
 }
+
 variable "oauth-token" {
-  type    = string
+  type        = string
+  description = "The token string you were given by your VCS provider."
 }
+
 variable "repo-identifier" {
-  type    = string
+  type        = string
+  description = "A reference to your VCS repository in the format <vcs organization>/<repository>."
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  description = "AWS access key - part of AWS credentials."
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "AWS secret access key - part of AWS credentials."
+  sensitive   = true
+}
+
+variable "AWS_DEFAULT_REGION" {
+  type        = string
+  description = "(Optional) The region where the aws resources will be created. Defaults to eu-central-1."
+  default     = "eu-central-1"
 }
