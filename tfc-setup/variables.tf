d123bf228dd57616afbbe165b4ba2d62b2cb575b
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-23 01:19:57 +0300

    added eks_cluster_name variable

--- tfc-setup/variables.tf
+++ tfc-setup/variables.tf
@@ -39,3 +39,8 @@ variable "AWS_DEFAULT_REGION" {
   description = "(Optional) The region where the aws resources will be created. Defaults to eu-central-1."
   default     = "eu-central-1"
 }
+
+variable "eks_cluster_name" {
+    type = string
+    description = "The name of your EKS cluster."
+}
