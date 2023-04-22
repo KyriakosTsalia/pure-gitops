57d6a6699f33f4172fc9ca2b106b3b4b1d0cc791
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-22 22:04:15 +0300

    add vpc

---
+++ eks-argocd/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~>1.4.4"
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~>0.44.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.64.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.AWS_REGION
+}
---
+++ eks-argocd/vpc.tf
@@ -0,0 +1,32 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+
+  name = "main"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  public_subnets  = [for index, _ in local.azs : "10.0.${index}.0/24"]
+  private_subnets = [for index, _ in local.azs : "10.0.${100 + index}.0/24"]
+
+  enable_nat_gateway   = true
+  enable_classiclink   = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    Name = "main-public"
+  }
+
+  private_subnet_tags = {
+    Name = "main-private"
+  }
+
+  vpc_tags = {
+    Name = "main"
+  }
+}
