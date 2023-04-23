5a1532a4c568ca317f3f31a27ca644202959bf61
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-23 17:50:00 +0300

    removed k8s provider

--- argocd/terraform.tf
+++ argocd/terraform.tf
@@ -32,13 +32,3 @@ provider "helm" {
     }
   }
 }
-
-provider "kubernetes" {
-  host                   = data.tfe_outputs.eks.values.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.tfe_outputs.eks.values.cluster_ca_cert_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "cluster-1"]
-    command     = "aws"
-  }
-}
