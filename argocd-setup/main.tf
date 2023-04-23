7f9ffb3776e0004dc5ceff33882ca3949d13bc5a
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-23 23:49:53 +0300

    removed vcs connection

--- argocd-setup/main.tf
+++ argocd-setup/main.tf
@@ -26,21 +26,21 @@ resource "gitlab_deploy_token" "argocd" {
   scopes = ["read_repository"]
 }
 
-resource "kubernetes_manifest" "private-repo-connection" {
-  manifest = {
-    "apiVersion" : "v1",
-    "kind" : "Secret",
-    "metadata" : {
-      "name" : "my-private-https-repo",
-      "namespace" : "argocd",
-      "labels" : {
-        "argocd.argoproj.io/secret-type" : "repository"
-      }
-    },
-    "stringData" : {
-      "url" : "https://gitlab.com/kyriakos_tsalia/pure-gitops.git",
-      "password" : gitlab_deploy_token.argocd.token,
-      "username" : gitlab_deploy_token.argocd.username,
-    }
-  }
-}
+# resource "kubernetes_manifest" "private-repo-connection" {
+#   manifest = {
+#     "apiVersion" : "v1",
+#     "kind" : "Secret",
+#     "metadata" : {
+#       "name" : "my-private-https-repo",
+#       "namespace" : "argocd",
+#       "labels" : {
+#         "argocd.argoproj.io/secret-type" : "repository"
+#       }
+#     },
+#     "data" : {
+#       "url" : bsae64encode("https://gitlab.com/kyriakos_tsalia/pure-gitops.git"),
+#       "password" : bsae64encode(gitlab_deploy_token.argocd.token),
+#       "username" : bsae64encode(gitlab_deploy_token.argocd.username),
+#     }
+#   }
+# }
