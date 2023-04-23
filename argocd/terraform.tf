f6184a343c2e31a0a625978e11a00bcee3f0b31f
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-23 14:43:40 +0300

    configured k8s provider

--- argocd/terraform.tf
+++ argocd/terraform.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~>2.9.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~>2.20.0"
+    }
     tfe = {
       source  = "hashicorp/tfe"
       version = "~>0.44.1"
@@ -26,5 +30,15 @@ provider "helm" {
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
       command     = "aws"
     }
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.tfe_outputs.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.tfe_outputs.eks.cluster_ca_cert_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
   }
 }
