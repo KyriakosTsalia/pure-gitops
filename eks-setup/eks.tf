c17b321967a8a316294097c9194e6a25e99c1fbb
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-26 15:48:25 +0300

    recreate aws vpc and eks

--- eks-setup/eks.tf
+++ eks-setup/eks.tf
@@ -1,47 +1,47 @@
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "19.13.1"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.13.1"
 
-#   cluster_name    = "cluster-1"
-#   cluster_version = "1.26"
+  cluster_name    = "cluster-1"
+  cluster_version = "1.26"
 
-#   cluster_endpoint_public_access  = true
-#   cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
-#   cluster_addons = {
-#     coredns = {
-#       most_recent = true
-#     }
-#     kube-proxy = {
-#       most_recent = true
-#     }
-#     vpc-cni = {
-#       most_recent = true
-#     }
-#   }
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
-#   vpc_id                   = module.vpc.vpc_id
-#   subnet_ids               = module.vpc.private_subnets
-#   control_plane_subnet_ids = module.vpc.public_subnets
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
 
-#   eks_managed_node_groups = {
-#     group-1 = {
-#       min_size         = 2
-#       max_size         = 2
-#       desired_size     = 2
-#       max_unavaileable = 1
+  eks_managed_node_groups = {
+    group-1 = {
+      min_size         = 2
+      max_size         = 2
+      desired_size     = 2
+      max_unavaileable = 1
 
-#       instance_types = ["t3.small"]
-#       capacity_type  = "SPOT"
-#       disk_size      = 50
-#     }
-#   }
-# }
+      instance_types = ["t3.small"]
+      capacity_type  = "SPOT"
+      disk_size      = 50
+    }
+  }
+}
 
-# output "cluster_endpoint" {
-#   value = module.eks.cluster_endpoint
-# }
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
 
-# output "cluster_ca_cert_data" {
-#   value = module.eks.cluster_certificate_authority_data
-# }
+output "cluster_ca_cert_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
