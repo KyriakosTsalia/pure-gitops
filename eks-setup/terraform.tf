48dbdd04534a1206854d0f9547a04d70be19cd1a
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-29 23:33:27 +0300

    remove terraform variable for aws_default_region, env variable is already set

--- eks-setup/terraform.tf
+++ eks-setup/terraform.tf
@@ -8,11 +8,4 @@ terraform {
   }
 }
 
-variable "AWS_DEFAULT_REGION" {
-  type        = string
-  description = "(Optional) The region where the aws resources will be created. Defaults to eu-central-1."
-}
-
-provider "aws" {
-  region = var.AWS_DEFAULT_REGION
-}
+provider "aws" {}
