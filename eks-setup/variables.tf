73f4afd5104cf76edc0ccd4e39f4c8b47619606d
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-29 19:04:09 +0300

    use AWS_DEFAULT_REGION tfc variable

--- eks-setup/variables.tf
+++ eks-setup/variables.tf
@@ -1,7 +1,7 @@
 variable "AWS_DEFAULT_REGION" {
   type        = string
   description = "(Optional) The region where the aws resources will be created. Defaults to eu-central-1."
-  default     = "eu-central-1"
+  default     = var.AWS_DEFAULT_REGION
 }
 
 variable "eks_cluster_name" {
