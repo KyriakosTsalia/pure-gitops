f97e12b024aa67ed60d4dc88e1d1eef0f39d1562
Kyriakos Tsaliagkos <kyriakos.tsaliagkos@gmail.com>
2023-04-29 22:43:14 +0300

    removed unused tfe provider

--- eks-setup/terraform.tf
+++ eks-setup/terraform.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = "~>1.4.4"
   required_providers {
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "~>0.44.1"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~>4.64.0"
